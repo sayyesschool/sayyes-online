50e696b3de60cd83c01bcd7fcca4874735026316
Oleg Polyakov <olegpolyakov@outlook.com>
2020-09-25 21:35:20 +0300

    Update api

--- api/index.js
+++ api/index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 
+const twilio = require('./twilio');
 const yandexKassa = require('./yandex-kassa');
 
 module.exports = core => {
     const api = express();
 
     api.use('/yandex-kassa', yandexKassa(core.services));
+    api.use('/twilio', twilio(core.lib.twilio));
 
     api.use((error, req, res, next) => {
-        res.status(500).send({ error: error.message });
+        console.log(error);
+        res.status(500).send({ error: error.message || error });
     });
 
     return api;
--- api/twilio.js
+++ api/twilio.js
@@ -1,6 +1,31 @@
 const router = require('express').Router();
 
-module.exports = ({ Ticket }) => {
+module.exports = twilio => {
+    router.get('/tokens/:type', (req, res, next) => {
+        const { type } = req.params;
+        const { identity, room, device = 'browser' } = req.query;
+        let token;
+
+        if (!identity) return next('Identity is not specified');
+
+        if (type === 'video') {
+            if (!room) return next('Room is not specified');
+
+            token = twilio.generateVideoToken({ identity, room });
+        } else if (type === 'chat') {
+            token = twilio.generateChatToken({ identity, device });
+        } else if (type === 'sync') {
+            token = twilio.generateSyncToken(identity);
+        }
+
+        res.json({
+            ok: true,
+            data: {
+                token
+            }
+        });
+    });
+
     router.post('/rooms', async (req, res) => {
 
     });
