2fb6ad8a06be5d0dc1152ba9ea34f5d535435a67
Oleg Polyakov <olegpolyakov@outlook.com>
2020-12-18 16:49:07 +0300

    Add index

---
+++ index.js
@@ -0,0 +1,28 @@
+const config = require('./config');
+const core = require('./core')(config);
+const db = require('./db');
+const server = require('./server')(config, db);
+const api = require('./api');
+const auth = require('./auth');
+const admin = require('./admin');
+const classroom = require('./class');
+const client = require('./client');
+const main = require('./main');
+const teacher = require('./teacher');
+
+const context = {
+    config,
+    ...core,
+    middleware: server.middleware
+};
+
+server.use('/api', api(context));
+server.use(auth(context));
+server.use('/admin', admin(context));
+server.use('/class', classroom(context));
+server.use('/teacher', teacher(context));
+server.use(client(context));
+server.use(main(context));
+
+db.connect(config.MONGODB_URI);
+server.listen(config.APP_PORT, () => console.log('Server started'));
