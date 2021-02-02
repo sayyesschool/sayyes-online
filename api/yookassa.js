9d78cc3bedfe26b91dc8cc23ca2550c8c375740d
Oleg Polyakov <olegpolyakov@outlook.com>
2021-02-02 10:28:21 +0300

    Update yookassa api

--- api/yookassa.js
+++ api/yookassa.js
@@ -1,30 +1,35 @@
 const router = require('express').Router();
 
-module.exports = ({ Ticket, Meeting, User }) => {
+module.exports = ({ Enrollment, Lesson, Ticket, User }) => {
     router.post('/payments', async (req, res, next) => {
         const event = req.body.event;
 
         if (event === 'payment.succeeded') {
             const payment = req.body.object;
             const user = await User.findById(payment.metadata.userId);
-            const meeting = await Meeting.findById(payment.metadata.meetingId);
 
-            const ticket = await Ticket.create({
-                user: user.id,
-                plan: payment.metadata.planId,
-                payment: {
-                    id: payment.id,
-                    amount: payment.amount.value,
-                    paid: payment.paid,
-                    date: payment.captured_at
-                }
-            }).then(ticket => {
-                res.sendStatus(200);
-                return ticket;
-            });
-
-            if (meeting) {
-                await Meeting.register(meeting, user, ticket);
+            if (payment.metadata.enrollmentId) {
+                Enrollment.findById(payment.metadata.enrollmentId)
+                    .then(enrollment => {
+                        const lessons = enrollment.createLessons(payment.metadata.packId);
+                        return Lesson.create(lessons);
+                    })
+                    .then(() => {
+                        res.sendStatus(200);
+                    });
+            } else if (payment.metadata.ticketId) {
+                Ticket.create({
+                    user: user.id,
+                    plan: payment.metadata.planId,
+                    payment: {
+                        id: payment.id,
+                        amount: payment.amount.value,
+                        paid: payment.paid,
+                        date: payment.captured_at
+                    }
+                }).then(() => {
+                    res.sendStatus(200);
+                });
             }
         } else if (event === 'refund.succeeded') {
             const refund = req.body.object;
