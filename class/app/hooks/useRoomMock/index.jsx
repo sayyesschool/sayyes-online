f7c14bc2ae8a72a7e8b5825ae773182c52e343aa
Oleg Polyakov <olegpolyakov@outlook.com>
2021-05-16 16:06:56 +0300

    Update class

---
+++ class/app/hooks/useRoomMock/index.jsx
@@ -0,0 +1,29 @@
+import EventEmitter from 'events';
+import { useRef, useState, useCallback } from 'react';
+
+export default function useRoom(localTracks, onError, options) {
+    const optionsRef = useRef(options);
+    const [room, setRoom] = useState(new EventEmitter());
+    const [isConnecting, setIsConnecting] = useState(false);
+
+    const connect = useCallback(token => {
+        const localParticipant = new EventEmitter();
+        localParticipant.sid = 'PA7f8315aa1599f67d724cfb0dd245280c';
+        localParticipant.audioTracks = new Map();
+        localParticipant.dataTracks = new Map();
+        localParticipant.identity = 'Олег';
+        localParticipant.networkQualityLevel = 5;
+        localParticipant.state = 'connected';
+        localParticipant.tracks = new Map();
+        localParticipant.videoTracks = new Map();
+
+        const room = new EventEmitter();
+        room.state = 'connected';
+        room.localParticipant = localParticipant;
+        room.participants = new Map();
+
+        setRoom(room);
+    }, [localTracks, onError]);
+
+    return { room, isConnecting, connect };
+}
