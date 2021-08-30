ddeaab47ad0b5378e1507b0e81726d95ea507945
Oleg Polyakov <olegpolyakov@outlook.com>
2021-08-30 18:49:28 +0800

    Update exercise model

--- core/models/course/exercise.js
+++ core/models/course/exercise.js
@@ -1,14 +1,28 @@
 const { Schema } = require('mongoose');
 
+const Item = new Schema({}, {
+    strict: false
+});
+
 const Exercise = new Schema({
-    slug: { type: String },
+    type: { type: String },
     title: { type: String },
     description: { type: String },
+    text: { type: String },
     image: { type: String },
     audio: { type: String },
     video: { type: String },
+    items: [Item],
     unit: { type: Schema.Types.ObjectId },
     lesson: { type: Schema.Types.ObjectId }
+});
+
+Exercise.virtual('uri').get(function() {
+    return `${this.parent().uri}/units/${this.unit}/lessons/${this.lesson}/exercises/${this.id}`;
+});
+
+Exercise.virtual('url').get(function() {
+    return this.uri;
 });
 
 Exercise.virtual('imageUrl').get(function() {
