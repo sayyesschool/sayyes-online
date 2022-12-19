e3d21a7fe48ca7252a5da13fc6452c3947dcd584
Oleg Polyakov <olegpolyakov@outlook.com>
2022-12-19 20:52:25 +0300

    Fix: useRoomContext to get participants

--- class/app/components/ParticipantAudioTracks/ParticipantAudioTracks.jsx
+++ class/app/components/ParticipantAudioTracks/ParticipantAudioTracks.jsx
@@ -1,6 +1,8 @@
-import useParticipants from 'app/hooks/useParticipants';
+import { memo } from 'react';
+
+import useRoomContext from 'app/hooks/useRoomContext';
 import useTracks from 'app/hooks/useTracks';
-import AudioTrack from 'app/components/AudioTrack/AudioTrack';
+import AudioTrack from 'app/components/AudioTrack';
 
 /*
   This ParticipantAudioTracks component will render the audio track for all participants in the room.
@@ -8,7 +10,7 @@ import AudioTrack from 'app/components/AudioTrack/AudioTrack';
   unnecessarily unmounted/mounted as the user switches between Gallery View and Speaker View.
 */
 export default function ParticipantAudioTracks() {
-    const participants = useParticipants();
+    const { participants } = useRoomContext();
 
     return participants.map(participant => (
         <Participant
@@ -18,11 +20,11 @@ export default function ParticipantAudioTracks() {
     ));
 }
 
-function Participant({ participant }) {
+const Participant = memo(function({ participant }) {
     const tracks = useTracks(participant);
     const audioTrack = tracks.find(track => track.kind === 'audio');
 
     if (audioTrack?.kind === 'audio') return <AudioTrack track={audioTrack} />;
 
     return null;
-}
+});
