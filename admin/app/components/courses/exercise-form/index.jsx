bc5791af46b140fb6d5db93942b3b353d2eb825c
Oleg Polyakov <olegpolyakov@outlook.com>
2023-01-19 13:30:16 +0800

    Add new exercise title options

--- admin/app/components/courses/exercise-form/index.jsx
+++ admin/app/components/courses/exercise-form/index.jsx
@@ -7,7 +7,9 @@ const defaultExercise = {
 };
 
 const titleOptions = [
+    'After you watch',
     'Before you begin',
+    'Before you watch',
     'Building language',
     'Building vocabulary',
     'Conversation strategy',
@@ -16,11 +18,13 @@ const titleOptions = [
     'Listening',
     'Listening & Speaking',
     'Listening & Strategies',
+    'Quiz',
     'Reading 1',
     'Reading 2',
     'Speaking naturally',
     'Strategy plus',
     'Talk about it',
+    'While you watch',
     'Writing & Speaking'
 ];
 
