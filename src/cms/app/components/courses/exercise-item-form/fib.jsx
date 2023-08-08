841c6c1b48a02abde8bcb3dacc88648f1567f634
Oleg Polyakov <olegpolyakov@outlook.com>
2023-08-08 13:15:24 +0300

    Add required prop to FIB

--- src/cms/app/components/courses/exercise-item-form/fib.jsx
+++ src/cms/app/components/courses/exercise-item-form/fib.jsx
@@ -1,18 +1,25 @@
-import { forwardRef, useImperativeHandle, useRef } from 'react';
+import { forwardRef, useCallback, useImperativeHandle, useRef, useState } from 'react';
 
 import ContentEditor from 'shared/components/content-editor';
-import { Text } from 'shared/ui-components';
+import { Checkbox, Flex, Text } from 'shared/ui-components';
 
-function FibItemForm({ content }, ref) {
+function FibItemForm({ content, required = false }, ref) {
     const editorRef = useRef();
+
+    const [isRequired, setRequired] = useState(required);
 
     useImperativeHandle(ref, () => ({
         get props() {
             return {
-                content: editorRef.current?.getData()
+                content: editorRef.current?.getData(),
+                required: isRequired
             };
         }
     }));
+
+    const handleChange = useCallback(event => {
+        setRequired(event.target.checked);
+    }, []);
 
     return (
         <>
@@ -21,7 +28,18 @@ function FibItemForm({ content }, ref) {
                 content={content}
             />
 
-            <Text px={2} py={1} as="small" type="body2">Для текстового поля используйте <code>{'{ответ 1|ответ 2|ответ 3}'}</code>.<br />Для выбора используйте <code>{'[ответ 1|ответ 2|ответ 3*]'}</code> (<sup>*</sup> отмечается правильный вариант).</Text>
+            <Flex direction="column" px={2} py={1} spacing={1}>
+                <Text as="small" type="body2">
+                    Для текстового поля используйте <code>{'{ответ 1|ответ 2|ответ 3}'}</code>.<br />
+                    Для выбора используйте <code>{'[ответ 1|ответ 2|ответ 3*]'}</code> (<sup>*</sup> отмечается правильный вариант).
+                </Text>
+
+                <Checkbox
+                    label="Обязательно для заполнения"
+                    checked={isRequired}
+                    onChange={handleChange}
+                />
+            </Flex>
         </>
     );
 }
