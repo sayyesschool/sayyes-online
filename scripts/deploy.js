064feee80c0b8d3751bda400d28398d05a254e9f
Oleg Polyakov <olegpolyakov@outlook.com>
2024-01-19 16:36:42 -0300

    Update deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,35 +1,33 @@
 const Ftp = require('ftp-deploy');
 
 const config = {
-    user: 'sayyes_fed37',
-    password: '.JwTAhLR2vpjq_K',
     host: 'sayyes.ftp.evennode.com',
     port: 21,
-    localRoot: process.cwd() + '/public',
-    remoteRoot: '/public',
-    include: ['*', '**/*'], // this would upload everything except dot files
+    user: 'sayyes_fed37',
+    password: '2FQ7uH!vqefbQ:B',
+    localRoot: process.cwd(),
+    remoteRoot: '/',
+    include: ['public/**/*'],
     deleteRemote: false,
     // Passive mode is forced (EPSV command is not sent)
     forcePasv: true,
-    sftp: true
+    sftp: false
 };
 
 const ftp = new Ftp();
 
-ftp.on('uploading', data => {
-    console.log('Total file count being transferred', data.totalFilesCount);
-    console.log('Number of files transferred', data.transferredFileCount);
-    console.log('Uploading', data.filename);
+ftp.on('uploaded', data => {
+    console.log(`Uploading ${data.filename} (${data.transferredFileCount}/${data.totalFilesCount})`);
 });
 
 ftp.on('log', data => {
-    console.log(data);
+    console.log('Log', data);
 });
 
-ftp.on('error', error => {
+ftp.on('upload-error', error => {
     console.log('Error', error);
 });
 
 ftp.deploy(config)
-    .then(res => console.log('Finished:', res))
-    .catch(err => console.log('Error', err));
+    .then(res => console.log('Finished uploading'))
+    .catch(err => console.log('Error uploading', err));
