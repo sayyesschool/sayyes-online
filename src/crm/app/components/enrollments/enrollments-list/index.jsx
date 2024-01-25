88ece41007a7b12b9021872a1b6dc4d2565d8195
Oleg Polyakov <olegpolyakov@outlook.com>
2024-01-25 04:10:53 -0300

    Update

--- src/crm/app/components/enrollments/enrollments-list/index.jsx
+++ src/crm/app/components/enrollments/enrollments-list/index.jsx
@@ -1,5 +1,7 @@
 import { Link } from 'react-router-dom';
 
+import { DomainLabel } from 'shared/data/common';
+import { StatusLabel } from 'shared/data/enrollment';
 import StatusIcon from 'shared/components/status-icon';
 import { Avatar, List, Text } from 'shared/ui-components';
 
@@ -15,8 +17,8 @@ export default function EnrollmentsList({ enrollments }) {
                         <StatusIcon status="info" />
                     }
                     content={<>
-                        <Text type="body1">{enrollment.domainLabel}</Text>
-                        <Text type="body2">{enrollment.statusLabel}</Text>
+                        <Text type="body-md">{DomainLabel[enrollment.domain]}</Text>
+                        <Text type="body-sm">{StatusLabel[enrollment.status]}</Text>
                     </>}
                     end={enrollment.teacher &&
                         <Avatar
