7d3084b52440868d3c46cfbf72fbb445e3d2c240
Oleg Polyakov <olegpolyakov@outlook.com>
2024-02-08 12:27:50 -0300

    Disable session cookie sameSite

--- src/server/middleware/session.js
+++ src/server/middleware/session.js
@@ -3,21 +3,21 @@ const expressSession = require('express-session');
 
 const MongoStore = connectMongo(expressSession);
 
-module.exports = ({ APP_ENV, APP_DOMAIN, SESSION_SECRET }, connection) => expressSession({
+module.exports = ({ APP_DOMAIN, SESSION_SECRET }, dbConnection) => expressSession({
+    name: 'sessionId',
     secret: SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    name: 'sessionId',
     cookie: {
         domain: `.${APP_DOMAIN}`,
         httpOnly: true,
-        secure: APP_ENV === 'production',
-        sameSite: 'Lax',
+        sameSite: false,
+        secure: true,
         signed: true,
         maxAge: 1000 * 60 * 60 * 24 * 3 // 3 days
     },
     store: new MongoStore({
-        mongooseConnection: connection,
+        mongooseConnection: dbConnection,
         ttl: 60 * 60 * 24 * 3, // 3 days
         touchAfter: 60 * 60 * 24, // 1 day
         stringify: false
