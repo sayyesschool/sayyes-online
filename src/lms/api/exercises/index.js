1590f7d65f7e56ba2baae9ce1ae82eeb2b7c1c28
Oleg Polyakov <olegpolyakov@outlook.com>
2024-04-07 10:36:38 -0300

    LMS: Add exercises router

---
+++ src/lms/api/exercises/index.js
@@ -0,0 +1,12 @@
+const { Router } = require('express');
+
+const Controller = require('./controller');
+
+module.exports = context => {
+    const router = Router();
+    const controller = Controller(context);
+
+    router.get('/:id', controller.getCourse);
+
+    return router;
+};
