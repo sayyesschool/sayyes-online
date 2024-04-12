aa7c354dccf0f1deebc6fa71da8fb66b18f95edc
Oleg Polyakov <olegpolyakov@outlook.com>
2024-04-12 11:54:50 -0300

    Remove sourcemap plugin

--- scripts/build/dev.js
+++ scripts/build/dev.js
@@ -1,4 +1,4 @@
-import { readFile, rm } from 'node:fs/promises';
+import { rm } from 'node:fs/promises';
 
 import { context } from 'esbuild';
 
@@ -9,23 +9,5 @@ await rm('./public', { recursive: true, force: true });
 await (await context({
     ...common,
     minify: false,
-    sourcemap: true,
-    plugins: [
-        ...common.plugins,
-        {
-            name: 'excludeVendorFromSourceMap',
-            setup(build) {
-                build.onLoad({ filter: /node_modules/ }, async args => {
-                    const contents = await readFile(args.path, 'utf8');
-
-                    return {
-                        loader: 'default',
-                        contents: args.path.endsWith('.json') ?
-                            contents :
-                            contents + '\n//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIiJdLCJtYXBwaW5ncyI6IkEifQ=='
-                    };
-                });
-            }
-        }
-    ]
+    sourcemap: true
 })).watch();
