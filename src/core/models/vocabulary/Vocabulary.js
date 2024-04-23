43c8ca6dda8412cf2769b2b78402cefbe3c65f0b
Oleg Polyakov <olegpolyakov@outlook.com>
2024-04-23 14:22:42 -0300

    Core: Add helper methods to Vocabulary model

--- src/core/models/vocabulary/Vocabulary.js
+++ src/core/models/vocabulary/Vocabulary.js
@@ -36,4 +36,19 @@ Vocabulary.virtual('lexemes', {
     foreignField: '_id'
 });
 
+Vocabulary.methods.addLexeme = async function(lexemeId) {
+    this.lexemeIds.push(lexemeId);
+
+    return this.save();
+};
+
+Vocabulary.methods.removeLexeme = async function(lexemeId) {
+    if (this.lexemeIds.includes(lexemeId))
+        throw new Error('Lexeme is not in the vocabulary');
+
+    this.lexemeIds.remove(lexemeId);
+
+    return this.save();
+};
+
 export default Vocabulary;
