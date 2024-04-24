45a2859aa6d8a4d15c6e54615c4d687b38a5997f
Oleg Polyakov <olegpolyakov@outlook.com>
2024-04-24 12:23:08 -0300

    LMS: Fix imports

--- src/lms/app/index.js
+++ src/lms/app/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import ErrorBoundary from 'shared/components/error-boundary';
 import { StoreProvider } from 'shared/store';
 import { theme, ThemeProvider } from 'shared/theme';
-
 import 'shared/styles/index.scss';
 
 import { App } from './components';
