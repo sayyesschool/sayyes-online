81d0080aa20f0895d2e82dd219d5df8e9cebad12
Oleg Polyakov <olegpolyakov@outlook.com>
2024-04-27 11:30:49 -0300

    Core: Add LexemeKind

--- src/core/models/lexeme/constants.js
+++ src/core/models/lexeme/constants.js
@@ -2,3 +2,16 @@ export const LexemeType = {
     Word: 'word',
     Phrase: 'phrase'
 };
+
+export const LexemeKind = {
+    Noun: 'noun',
+    Verb: 'verb',
+    Adjective: 'adjective',
+    Adverb: 'adverb',
+    Pronoun: 'pronoun',
+    Preposition: 'preposition',
+    Conjunction: 'conjunction',
+    Interjection: 'Interjection',
+    Determiner: 'determiner',
+    Article: 'article'
+};
