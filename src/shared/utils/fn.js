7cbf9e2dc1e48dca97fe18c2620b5e0f09e39f69
Oleg Polyakov <olegpolyakov@outlook.com>
2024-04-28 10:59:55 -0300

    Add debounce and delay utils

--- src/shared/utils/fn.js
+++ src/shared/utils/fn.js
@@ -1,16 +1,58 @@
-export function throttle(fn, ms) {
+/**
+ * Groups sequential function calls into one.
+ * @param {Function} fn
+ * @param {number} delay
+ * @returns {Function}
+ */
+export function debounce(fn, delay = 0) {
+    let timeout;
+
+    return function debounced(...args) {
+        debounced.cancel = () => clearTimeout(timeout);
+
+        clearTimeout(timeout);
+
+        timeout = setTimeout(() => {
+            fn(...args);
+        }, delay);
+    };
+}
+
+/**
+ * Delays executing a function.
+ * @param {Function} fn
+ * @param {number} delay
+ * @returns {Function}
+ */
+export function throttle(fn, delay = 0) {
     let timeout;
     let cache = {};
+    let canceled = false;
 
-    return (...args) => {
+    return function throttled(...args) {
+        throttled.cancel = () => {
+            clearTimeout(timeout);
+            timeout = null;
+            canceled = true;
+        };
+
         cache.lastArgs = args;
 
-        if (!timeout) {
-            timeout = setTimeout(() => {
-                fn(...cache.lastArgs);
-                clearTimeout(timeout);
-                timeout = null;
-            }, ms);
-        }
+        if (timeout || canceled) return;
+
+        timeout = setTimeout(() => {
+            fn(...cache.lastArgs);
+            clearTimeout(timeout);
+            timeout = null;
+        }, delay);
     };
+}
+
+export function delay(fn, ms = 0) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            fn();
+            resolve();
+        }, ms);
+    });
 }
