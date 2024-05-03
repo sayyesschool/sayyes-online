a012320979f10016adedd5bbce9b91d1841f1e53
Oleg Polyakov <olegpolyakov@outlook.com>
2024-05-03 11:42:51 -0300

    refactor: Add error handling for invalid vocabulary ID

--- src/lms/api/vocabularies/middleware.js
+++ src/lms/api/vocabularies/middleware.js
@@ -1,13 +1,20 @@
+import { isObjectIdOrHexString } from 'mongoose';
+
 export default ({
     models: { Vocabulary }
 }) => ({
     async findOne(req, res, next, id) {
+        if (!isObjectIdOrHexString(id)) next({
+            code: 400,
+            message: 'Неверный идентификатор'
+        });
+
         const vocabulary = await Vocabulary.findById(id);
 
-        if (!vocabulary) throw {
+        if (!vocabulary) next({
             code: 404,
             message: 'Словарь не найден'
-        };
+        });
 
         req.vocabulary = vocabulary;
 
