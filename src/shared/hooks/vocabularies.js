4ffbb1bca5b9cd65109120053a1167953641e529
Oleg Polyakov <olegpolyakov@outlook.com>
2024-06-05 12:17:30 +0300

    refactor: Remove useVocabularySearch hook

--- src/shared/hooks/vocabularies.js
+++ src/shared/hooks/vocabularies.js
@@ -1,37 +1,8 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
 import { useActions, useStore } from 'shared/hooks/store';
-import http from 'shared/services/http';
 import { actions as vocabulariesActions } from 'shared/store/modules/vocabularies';
 import { hasKey } from 'shared/utils/object';
-
-export function useVocabularySearch(query) {
-    const [results, setResults] = useState();
-    const [isLoading, setLoading] = useState(false);
-    const [hasMore, setHasMore] = useState(false);
-
-    const search = useCallback(() => {
-        setLoading(true);
-        const qs = new URLSearchParams(query).toString();
-        http.get('/vocabulary/search?' + qs)
-            .then(response => {
-                setResults(response.data);
-                setHasMore(response.meta.more);
-
-                return response;
-            })
-            .finally(() => {
-                setLoading(false);
-            });
-    }, [query]);
-
-    return {
-        search,
-        results,
-        isLoading,
-        hasMore
-    };
-}
 
 export function useVocabularies(query) {
     const [vocabularies, actions] = useStore(
