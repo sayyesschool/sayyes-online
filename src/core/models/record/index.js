fb8944447b86a940fc6ba361ace9ca622b0b5c98
Oleg Polyakov <olegpolyakov@outlook.com>
2024-07-17 22:45:32 +0300

    Fix discriminator type for LexemeRecord

--- src/core/models/record/index.js
+++ src/core/models/record/index.js
@@ -4,6 +4,6 @@ import LexemeRecordSchema from './LexemeRecord';
 import RecordSchema from './Record';
 
 export const Record = model('Record', RecordSchema);
-export const LexemeRecord = Record.discriminator('LexemeRecord', LexemeRecordSchema, 'lexicon');
+export const LexemeRecord = Record.discriminator('LexemeRecord', LexemeRecordSchema, 'lexeme');
 
 export default Record;
