b38d9fd61adc5011360e7fa634b98866dcf39584
Oleg Polyakov <olegpolyakov@outlook.com>
2024-08-22 12:42:58 +0800

    Small fixes to SearchFrom

--- src/shared/ui-components/form/FormInput.jsx
+++ src/shared/ui-components/form/FormInput.jsx
@@ -1,15 +1,30 @@
+import { forwardRef } from 'react';
+
 import cn from 'classnames';
 
 import Input from '../input/Input';
 
 import FormField from './FormField';
 
-export default function FormInput({ label, message, className, ...props }) {
-    const classNames = cn('ui-FormSelect', className);
+const FormInput = forwardRef(({
+    label,
+    message,
+    className,
+    ...props
+}, ref) => {
+    const classNames = cn('ui-FormInput', className);
 
     return (
-        <FormField className={classNames} label={label} message={message}>
-            <Input {...props} />
+        <FormField
+            className={classNames}
+            label={label}
+            message={message}
+        >
+            <Input ref={ref} {...props} />
         </FormField>
     );
-}
+});
+
+FormInput.displayName = 'FormInput';
+
+export default FormInput;
