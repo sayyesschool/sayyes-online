481ae8b1386d44a5b22c96eb43fb81d4cbc6bb4d
Oleg Polyakov <olegpolyakov@outlook.com>
2024-09-04 23:12:13 +0800

    Add app notification to lk

--- src/lk/app/store/reducer.js
+++ src/lk/app/store/reducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
+import notification from 'shared/store/modules/notification';
 import user from 'shared/store/modules/user';
 
 export default combineReducers({
+    notification,
     user
 });
