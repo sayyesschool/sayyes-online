edc77d83e87cab0099de6270e2475356d29d3292
Oleg Polyakov <olegpolyakov@outlook.com>
2024-11-13 15:22:09 -0300

    feat: update cron job to send meeting reminders using Club service with moment.js

--- src/cron.js
+++ src/cron.js
@@ -1,12 +1,20 @@
 import { CronJob } from 'cron';
+import moment from 'moment';
 
 import config from './config';
 import core from './core';
 
-const { services: { Meeting } } = core(config);
+const { services: { Club } } = core(config);
 
 new CronJob({
     cronTime: '0 0 9-21 * * *',
-    onTick: () => Meeting.notifyRegistrants(),
+    onTick: async () => {
+        const now = moment().utc().minutes(0).seconds(0).milliseconds(0);
+        const inADay = now.add(1, 'day');
+        const inAnHour = now.add(1, 'hour');
+
+        Club.sendMeetingsReminders(inADay, { templateId: 1348661 });
+        Club.sendMeetingsReminders(inAnHour, { templateId: 1348680 });
+    },
     start: true
 });
