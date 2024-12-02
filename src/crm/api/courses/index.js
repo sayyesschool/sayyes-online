359713749876a850fc32874ec8c262d74b9408eb
Oleg Polyakov <olegpolyakov@outlook.com>
2024-12-02 20:24:59 -0300

    feat: fix route syntax for course units, lessons, exercises, and items for proper API functionality

--- src/crm/api/courses/index.js
+++ src/crm/api/courses/index.js
@@ -15,23 +15,23 @@ export default context => {
         .put(courses.update)
         .delete(courses.delete);
 
-    router.route('/:course/units/{:unit}')
+    router.route('/:course/units{/:unit}')
         .post(units.create)
         .put(units.update)
         .delete(units.delete);
 
-    router.route('/:course/lessons/{:lesson}')
+    router.route('/:course/lessons{/:lesson}')
         .post(lessons.create)
         .put(lessons.update)
         .delete(lessons.delete);
 
-    router.route('/:course/exercises/{:exercise}')
+    router.route('/:course/exercises{/:exercise}')
         .get(exercises.getOne)
         .post(exercises.create)
         .put(exercises.update)
         .delete(exercises.delete);
 
-    router.route('/:course/exercises/:exercise/items/{:item}')
+    router.route('/:course/exercises/:exercise/items{/:item}')
         .post(items.create)
         .put(items.update)
         .delete(items.delete);
