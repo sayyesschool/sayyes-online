a13a532bbd6a097bdf560bcc74cc72dd24be9788
Oleg Polyakov <olegpolyakov@outlook.com>
2024-12-17 21:08:37 -0300

    feat: refactor ErrorBoundary component to improve import order and maintainability

--- src/shared/components/error-boundary/ErrorBoundary.jsx
+++ src/shared/components/error-boundary/ErrorBoundary.jsx
@@ -1,12 +1,12 @@
 import {
-    init,
     BrowserTracing,
-    Replay,
-    ErrorBoundary as SentryErrorBoundary
+    ErrorBoundary as SentryErrorBoundary,
+    init,
+    Replay
 } from '@sentry/react';
 
-import { APP_ENV, SENTRY_DSN } from 'shared/constants';
 import ErrorState from 'shared/components/error-state';
+import { APP_ENV, SENTRY_DSN } from 'shared/constants';
 
 if (APP_ENV === 'production') {
     init({
@@ -16,7 +16,7 @@ if (APP_ENV === 'production') {
         tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
         // Session Replay
         replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-        replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+        replaysOnErrorSampleRate: 1.0 // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
     });
 }
 
