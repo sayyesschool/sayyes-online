84d303f3f10ecaa2212fb54a9a5ed34814e7da1a
Oleg Polyakov <olegpolyakov@outlook.com>
2024-12-17 21:09:12 -0300

    feat: enhance storage middleware to handle delete requests and improve error notifications

--- src/shared/store/middleware/storage.js
+++ src/shared/store/middleware/storage.js
@@ -5,7 +5,33 @@ const storageMiddleware = store => next => action => {
 
     const { method, body } = action.request;
 
-    if (method === 'post' || method === 'put') {
+    const isDelete = method === 'delete';
+    const isPost = method === 'post';
+    const isPut = method === 'put';
+
+    if (
+        isDelete ||
+        ((isPost || isPut) && body.file.delete)
+    ) {
+        return Storage.delete(body.file.url)
+            .then(() => {
+                delete body.file;
+
+                return next(action);
+            })
+            .catch(error => {
+                next({ type: 'HIDE_NOTIFICATION' });
+
+                next({
+                    type: 'SHOW_NOTIFICATION',
+                    notification: {
+                        active: true,
+                        type: 'error',
+                        text: error.message || error
+                    }
+                });
+            });
+    } else if (isPost || isPut) {
         const file = body.file;
 
         return Storage.upload(file, {
@@ -14,6 +40,11 @@ const storageMiddleware = store => next => action => {
         }).then(response => {
             if (typeof body.props === 'object') {
                 body.props.path = response.data.path;
+            }
+
+            if (typeof body.image === 'object') {
+                body.image.path = response.data.path;
+                body.image.url = response.data.url;
             }
 
             delete body.file;
@@ -31,21 +62,6 @@ const storageMiddleware = store => next => action => {
                 }
             });
         });
-    } else if (method === 'delete') {
-        return Storage.delete(body.file.url)
-            .then(() => next(action))
-            .catch(error => {
-                next({ type: 'HIDE_NOTIFICATION' });
-
-                next({
-                    type: 'SHOW_NOTIFICATION',
-                    notification: {
-                        active: true,
-                        type: 'error',
-                        text: error.message || error
-                    }
-                });
-            });
     } else {
         return next(action);
     }
