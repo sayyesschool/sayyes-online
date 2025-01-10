d1e346374caccd7e06a2c55e67b6dd7bc1f29226
Oleg Polyakov <olegpolyakov@outlook.com>
2025-01-10 17:58:17 -0300

    feat: enhance import map resolution and simplify import paths

--- loaders/import-map.mjs
+++ loaders/import-map.mjs
@@ -1,25 +1,26 @@
-import { join } from 'node:path';
-
 const ROOT_PATH = process.cwd();
 
 const imports = {
-    'config': `${ROOT_PATH}/src/config.js`,
-    'core': `${ROOT_PATH}/src/core/index.js`,
-    'db': `${ROOT_PATH}/src/db.js`,
-    'auth': `${ROOT_PATH}/src/auth/index.js`,
-    'club': `${ROOT_PATH}/src/club/index.js`,
-    'cms': `${ROOT_PATH}/src/cms/index.js`,
-    'crm': `${ROOT_PATH}/src/crm/index.js`,
-    'lms': `${ROOT_PATH}/src/lms/index.js`
+    config: `${ROOT_PATH}/src/config.js`,
+    db: `${ROOT_PATH}/src/db.js`,
+    core: `${ROOT_PATH}/src/core`,
+    auth: `${ROOT_PATH}/src/auth`,
+    club: `${ROOT_PATH}/src/club`,
+    cms: `${ROOT_PATH}/src/cms`,
+    crm: `${ROOT_PATH}/src/crm`,
+    lms: `${ROOT_PATH}/src/lms`,
+    shared: `${ROOT_PATH}/src/shared/`
 };
 
 export async function resolve(specifier, context, nextResolve) {
-    if (specifier.startsWith('@/')) {
-        specifier = specifier.replace('@', join(ROOT_PATH, 'src'));
+    let [name, ...rest] = specifier.split('/');
+
+    if (name === '@') {
+        name = rest.shift();
     }
 
-    if (Object.hasOwn(imports, specifier)) {
-        return nextResolve(imports[specifier], context);
+    if (name in imports) {
+        return nextResolve(imports[name] + rest.join('/'), context);
     }
 
     return nextResolve(specifier, context);
