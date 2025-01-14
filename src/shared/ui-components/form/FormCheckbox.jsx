7f7675fc11946af37f783aaf9429c39b071dc7ba
Oleg Polyakov <olegpolyakov@outlook.com>
2025-01-13 23:30:04 -0300

    feat: set default orientation to 'horizontal' in FormCheckbox and FormSwitch components

--- src/shared/ui-components/form/FormCheckbox.jsx
+++ src/shared/ui-components/form/FormCheckbox.jsx
@@ -2,7 +2,7 @@ import Checkbox from '../checkbox/Checkbox';
 
 import FormField from './FormField';
 
-export default function FormCheckbox({ label, orientation, ...props }) {
+export default function FormCheckbox({ label, orientation = 'horizontal', ...props }) {
     return (
         <FormField label={label} orientation={orientation}>
             <Checkbox {...props} />
--- src/shared/ui-components/form/FormSwitch.jsx
+++ src/shared/ui-components/form/FormSwitch.jsx
@@ -2,7 +2,7 @@ import Switch from '../switch/Switch';
 
 import FormField from './FormField';
 
-export default function FormSwitch({ label, orientation, ...props }) {
+export default function FormSwitch({ label, orientation = 'horizontal', ...props }) {
     return (
         <FormField label={label} orientation={orientation}>
             <Switch {...props} />
