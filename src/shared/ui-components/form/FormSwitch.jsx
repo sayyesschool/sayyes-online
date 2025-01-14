2efdb8a3360c348b43eca5cb0fbe4b350777e7d7
Oleg Polyakov <olegpolyakov@outlook.com>
2025-01-13 22:35:21 -0300

    feat: add orientation prop to form components for flexible layout adjustments

--- src/shared/ui-components/form/FormSwitch.jsx
+++ src/shared/ui-components/form/FormSwitch.jsx
@@ -2,9 +2,9 @@ import Switch from '../switch/Switch';
 
 import FormField from './FormField';
 
-export default function FormSwitch({ label, ...props }) {
+export default function FormSwitch({ label, orientation, ...props }) {
     return (
-        <FormField label={label} orientation="horizontal">
+        <FormField label={label} orientation={orientation}>
             <Switch {...props} />
         </FormField>
     );
