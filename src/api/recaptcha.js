eb01bbbf35e227edaab7749e1325682fb99ee157
Oleg Polyakov <olegpolyakov@outlook.com>
2025-01-15 15:49:45 -0300

    feat: simplify recaptcha verification request by removing unnecessary logging and using query parameters

--- src/api/recaptcha.js
+++ src/api/recaptcha.js
@@ -8,32 +8,15 @@ export default ({
     router.post('/', async (req, res) => {
         const { token } = req.body;
 
-        console.log({
-            secret: RECAPTCHA_SECRET_KEY,
-            response: token
-        });
-
         if (!token) {
             return res.status(400).json({ success: false, error: 'Не указан токен' });
         }
 
-        const data = await fetch('https://www.google.com/recaptcha/api/siteverify', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                secret: RECAPTCHA_SECRET_KEY,
-                response: token
-            })
+        const data = await fetch(`https://www.google.com/recaptcha/api/siteverify?secret=${RECAPTCHA_SECRET_KEY}&response=${token}`, {
+            method: 'POST'
         }).then(res => res.json());
 
-        console.log(data);
-
-        return res.json({
-            success: data.success,
-            score: data.score
-        });
+        return res.json(data);
     });
 
     return router;
