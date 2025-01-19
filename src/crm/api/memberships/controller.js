ab5eaf5a1532886bce4218ee175367d01b572b15
Oleg Polyakov <olegpolyakov@outlook.com>
2025-01-19 19:54:48 -0300

    feat: implement CRUD operations for Membership model in new router structure

--- src/crm/api/memberships/controller.js
+++ src/crm/api/memberships/controller.js
@@ -7,7 +7,7 @@ export default ({
 
         res.json({
             ok: true,
-            data: memberships.map(m => m.toObject())
+            data: memberships
         });
     },
 
@@ -17,7 +17,41 @@ export default ({
 
         res.json({
             ok: true,
-            data: membership.toObject()
+            data: membership
+        });
+    },
+
+    async create(req, res) {
+        const membership = await Membership.create(req.body);
+
+        await membership.populate('user', 'firstname lastname email');
+
+        res.json({
+            ok: true,
+            message: 'Абонемент создан',
+            data: membership
+        });
+    },
+
+    async update(req, res) {
+        const membership = await Membership.update(req.params.id, req.body, { new: true });
+
+        res.json({
+            ok: true,
+            message: 'Абонемент изменен',
+            data: membership
+        });
+    },
+
+    async delete(req, res) {
+        const membership = await Membership.delete(req.params.id);
+
+        res.json({
+            ok: true,
+            message: 'Абонемент удален',
+            data: {
+                id: membership.id
+            }
         });
     }
 });
