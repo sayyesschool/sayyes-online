46ad20a13e958e46676eb2d6a48e833020e38e22
Oleg Polyakov <olegpolyakov@outlook.com>
2025-01-19 19:53:14 -0300

    feat: update cron job to run every 30 minutes and improve meeting reminders logic

--- src/cron.js
+++ src/cron.js
@@ -4,18 +4,27 @@ import datetime from 'shared/libs/datetime';
 
 import config from './config';
 import core from './core';
+import db from './db';
 
 const { services: { Club } } = core(config);
 
 new CronJob({
-    cronTime: '0 0 9-21 * * *',
+    cronTime: '0 0/30 * * * *', // every 30 minutes
+    start: true,
     onTick: async () => {
-        const now = datetime().utc().minutes(0).seconds(0).milliseconds(0);
-        const inADay = now.add(1, 'day');
-        const inAnHour = now.add(1, 'hour');
+        console.log('Cron job running...');
 
-        Club.sendMeetingsReminders(inADay, { templateId: 1348661 });
-        Club.sendMeetingsReminders(inAnHour, { templateId: 1348680 });
-    },
-    start: true
+        await db.connect(config.DB_CONNECTION_STRING);
+
+        const now = datetime().utc().seconds(0).milliseconds(0);
+        const hourBefore = now.clone().add(1, 'hour').toDate();
+        const dayBefore = now.clone().add(1, 'day').toDate();
+
+        await Club.sendMeetingsReminders({ startDate: hourBefore }, { templateId: 1348680 });
+        await Club.sendMeetingsReminders({ startDate: dayBefore }, { templateId: 1348661 });
+
+        await db.disconnect();
+
+        console.log('Cron job finished...');
+    }
 });
