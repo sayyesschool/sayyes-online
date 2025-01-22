13051a380388561f1963dc2b614c85fbd0171825
Oleg Polyakov <olegpolyakov@outlook.com>
2025-01-22 09:32:47 -0300

    refactor: convert Learner controller methods to async/await and move router logic to a separate file

--- src/crm/api/learners/controller.js
+++ src/crm/api/learners/controller.js
@@ -1,79 +1,70 @@
 export default ({
     models: { Learner }
 }) => ({
-    get: (req, res, next) => {
-        const query = req.query ? req.query : {};
+    async get(req, res) {
+        const learners = await Learner.find(req.query ?? {});
 
-        Learner.find(query)
-            .then(learners => {
-                res.json({
-                    ok: true,
-                    data: learners
-                });
-            })
-            .catch(next);
+        res.json({
+            ok: true,
+            data: learners
+        });
     },
 
-    getOne: (req, res, next) => {
-        Learner.findById(req.params.id)
+    async getOne(req, res) {
+        const learner = await Learner.findById(req.params.id)
             .populate({
                 path: 'requests',
-                select: 'status manager contact',
-                populate: { path: 'manager', select: 'firstname lastname' }
+                select: 'status manager contact'
+                // populate: { path: 'manager', select: 'firstname lastname' }
             })
+            .populate('memberships')
             .populate({
                 path: 'enrollments',
-                select: 'status type domain format manager schedules',
-                populate: { path: 'manager', select: 'firstname lastname' }
+                select: 'status type domain format manager schedules'
+                // populate: { path: 'manager', select: 'firstname lastname' }
             })
             .populate({
                 path: 'transactions',
                 populate: { path: 'enrollment', select: 'domain' }
-            })
-            .then(learner => {
-                res.json({
-                    ok: true,
-                    data: learner
-                });
-            })
-            .catch(next);
+            });
+
+        res.json({
+            ok: true,
+            data: learner
+        });
     },
 
-    create: (req, res, next) => {
-        Learner.create(req.body)
-            .then(learner => {
-                res.json({
-                    ok: true,
-                    message: 'Ученик создан',
-                    data: learner
-                });
-            })
-            .catch(next);
+    async create(req, res) {
+        const learner = await Learner.create(req.body);
+
+        res.json({
+            ok: true,
+            message: 'Ученик создан',
+            data: learner
+        });
     },
 
-    update: (req, res, next) => {
-        Learner.findByIdAndUpdate(req.params.id, req.body, { new: true })
-            .then(learner => {
-                res.json({
-                    ok: true,
-                    message: 'Ученик изменен',
-                    data: learner
-                });
-            })
-            .catch(next);
+    async update(req, res) {
+        const learner = await Learner.findByIdAndUpdate(req.params.id, req.body, {
+            new: true
+        });
+
+        res.json({
+            ok: true,
+            message: 'Ученик изменен',
+            data: learner
+        });
     },
 
-    delete: (req, res, next) => {
-        Learner.findByIdAndDelete(req.params.id)
-            .then(learner => {
-                res.json({
-                    ok: true,
-                    message: 'Ученик удален',
-                    data: {
-                        id: learner.id
-                    }
-                });
-            })
-            .catch(next);
+    async delete(req, res) {
+        const learner = await Learner.findByIdAndDelete(req.params.id);
+
+        res.json({
+            ok: true,
+            message: 'Ученик удален',
+            data: {
+                id: learner.id
+            }
+        });
     }
 });
