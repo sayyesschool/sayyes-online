7f4865f383e7d79a04b381ea58ce56cea6f4295b
Oleg Polyakov <olegpolyakov@outlook.com>
2025-01-28 18:42:57 -0300

    feat: add test data for users, meetings, memberships, packs, payments, and requests

---
+++ test/data/meetings.js
@@ -0,0 +1,13 @@
+export const DEFAULT_MEETING = {
+    title: 'Test Meeting'
+};
+
+export const FREE_MEETING = {
+    title: 'Test Meeting',
+    free: true
+};
+
+export const ZOOM_MEETING = {
+    ...DEFAULT_MEETING,
+    zoomId: '1234567890'
+};
