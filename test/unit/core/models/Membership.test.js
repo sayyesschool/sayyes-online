f2e2f0e346549895410aea4ef8f674e2be2fc2f8
Oleg Polyakov <olegpolyakov@outlook.com>
2025-01-29 13:02:23 -0300

    feat: add unit tests for Membership model

---
+++ test/unit/core/models/Membership.test.js
@@ -0,0 +1,11 @@
+import expect from 'expect';
+import { model } from 'mongoose';
+import sinon from 'sinon';
+
+import { MembershipSchema } from 'core/models/membership';
+
+const Membership = model('Membership', MembershipSchema);
+
+describe('Membership model', () => {
+
+});
