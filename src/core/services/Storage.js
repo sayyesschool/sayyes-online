863e76793487a11f9ce4ee62c5d44cff8377facd
Oleg Polyakov <olegpolyakov@outlook.com>
2025-02-16 00:07:28 -0300

    feat: enhance Storage service with unique filename generation and path normalization

--- src/core/services/Storage.js
+++ src/core/services/Storage.js
@@ -1,4 +1,27 @@
-export default ({ clients: { storage } }) => ({
+import { basename, extname, join } from 'node:path/posix';
+
+import { v4 as uuid } from 'uuid';
+
+export default ({
+    config: { STORAGE_URL },
+    clients: { storage }
+}) => ({
+    isFilename(path) {
+        return basename(path).includes('.');
+    },
+
+    getUniqueFilename(file) {
+        return uuid() + extname(file.originalname);
+    },
+
+    getNormalizedPath(path, file) {
+        if (path.includes(STORAGE_URL)) {
+            path = new URL(path).pathname.split('/').slice(2).join('/');
+        }
+
+        return this.isFilename(path) ? path : join(path, this.getUniqueFilename(file));
+    },
+
     put: storage.put,
     delete: storage.delete
 });
