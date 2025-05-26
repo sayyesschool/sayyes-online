4e691b8ac3dbd3c4aa7176db6b357c1499d27d40
Oleg Polyakov <olegpolyakov@outlook.com>
2025-05-26 19:05:05 -0300

    feat: add navigation items to AppHeader based on user domains

--- src/lk/app/components/App.jsx
+++ src/lk/app/components/App.jsx
@@ -2,9 +2,12 @@ import { Route, Switch } from 'react-router-dom';
 
 import AppContent from 'shared/components/app-content';
 import AppHeader from 'shared/components/app-header';
+import AppNav from 'shared/components/app-nav';
 import AppNotification from 'shared/components/app-notification';
 import AppShell from 'shared/components/app-shell';
 import LoadingIndicator from 'shared/components/loading-indicator';
+import { APP_DOMAIN } from 'shared/constants';
+import { UserDomainLabel } from 'shared/data/user';
 import { useUser } from 'shared/hooks/user';
 
 import Account from 'lk/components/account';
@@ -16,11 +19,26 @@ export default function App() {
 
     if (!user) return <LoadingIndicator fullscreen />;
 
+    const navItems = user.domains
+        .filter(domain => domain !== 'lk')
+        .map(domain => ({
+            key: domain,
+            as: 'a',
+            href: `//${domain}.${APP_DOMAIN}`,
+            text: UserDomainLabel[domain]
+        }));
+
     return (
         <AppShell className={styles.root}>
             <AppHeader
                 user={user}
-            />
+            >
+                <AppNav
+                    items={navItems}
+                    orientation="horizontal"
+                    invertedColors
+                />
+            </AppHeader>
 
             <AppContent>
                 <Switch>
