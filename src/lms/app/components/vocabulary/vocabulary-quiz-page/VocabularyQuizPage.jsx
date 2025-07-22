25835a9e177362ad835118f358241324869ac037
Andrey <raigon28@gmail.com>
2025-07-22 04:30:25 +0300

    fix: update lexeme status in trainer

--- src/lms/app/components/vocabulary/vocabulary-quiz-page/VocabularyQuizPage.jsx
+++ src/lms/app/components/vocabulary/vocabulary-quiz-page/VocabularyQuizPage.jsx
@@ -3,6 +3,7 @@ import { useHistory } from 'react-router-dom';
 
 import LoadingIndicator from 'shared/components/loading-indicator';
 import Page from 'shared/components/page';
+import { useUser } from 'shared/hooks/user';
 import { useVocabulary } from 'shared/hooks/vocabularies';
 
 import VocabularyQuiz from 'lms/components/vocabulary/vocabulary-quiz';
@@ -10,13 +11,17 @@ import VocabularyQuiz from 'lms/components/vocabulary/vocabulary-quiz';
 import styles from './VocabularyQuizPage.module.scss';
 
 export default function VocabularyQuizPage({ match }) {
+    const [user] = useUser();
     const history = useHistory();
-
     const [vocabulary, actions] = useVocabulary(match.params.vocabulary);
 
     const handleBack = useCallback(() => {
         history.push(`/vocabularies/${vocabulary.id}`);
     }, [history, vocabulary?.id]);
+
+    const handleUpdateLexemeStatus = useCallback((id, newStatus) => {
+        actions.updateLexemeStatus(id, { learnerId: user.id, status: newStatus });
+    }, [actions, user.id]);
 
     if (!vocabulary) return <LoadingIndicator />;
 
@@ -25,7 +30,7 @@ export default function VocabularyQuizPage({ match }) {
             <VocabularyQuiz
                 vocabulary={vocabulary}
                 quizType={match.params.quiz}
-                updateLexemeStatus={actions.updateLexemeStatus}
+                updateLexemeStatus={handleUpdateLexemeStatus}
                 onBack={handleBack}
             />
         </Page>
