6b32d93e31ad5e836e120ab22829532d480cc276
Oleg Polyakov <olegpolyakov@outlook.com>
2025-07-24 19:17:22 +0300

    shared: Fix deps array

--- src/shared/hooks/fn.js
+++ src/shared/hooks/fn.js
@@ -2,7 +2,7 @@ import { useMemo, useRef } from 'react';
 
 import { debounce, throttle } from '@/shared/utils/fn';
 
-export function useDebounce(fn, delay, ...deps) {
+export function useDebounce(fn, delay, deps = []) {
     const fnRef = useRef(fn);
 
     const debounced = useMemo(() => debounce(fnRef.current, delay), [delay, ...deps]);
@@ -10,7 +10,7 @@ export function useDebounce(fn, delay, ...deps) {
     return debounced;
 }
 
-export function useThrottle(fn, delay, ...deps) {
+export function useThrottle(fn, delay, deps = []) {
     const fnRef = useRef(fn);
 
     const throttled = useMemo(() => throttle(fnRef.current, delay), [delay, ...deps]);
