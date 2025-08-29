3f1a74c021d8b550bc1b2dcb286248cdf5ae82cc
Oleg Polyakov <olegpolyakov@outlook.com>
2025-08-29 23:27:27 +0300

    fix: Add default parameters to utility functions in object.js

--- src/shared/utils/object.js
+++ src/shared/utils/object.js
@@ -4,15 +4,15 @@ export function isObject(arg) {
     return arg !== null && typeof arg === 'object';
 }
 
-export function isObjectEmpty(object) {
+export function isObjectEmpty(object = {}) {
     return Object.keys(object).length === 0;
 }
 
-export function hasKey(object, key) {
+export function hasKey(object = {}, key) {
     return !!object && key in object;
 }
 
-export function pick(object, keys = []) {
+export function pick(object = {}, keys = []) {
     const entries = Object.entries(object)
         .filter(([key]) => keys.includes(key));
     const map = new Map(entries);
@@ -20,15 +20,15 @@ export function pick(object, keys = []) {
     return toObject(map.entries());
 }
 
-export function stripEmptyValues(object) {
+export function stripEmptyValues(object = {}) {
     return pick(object, Object.keys(object).filter(key => object[key] != null && object[key] !== ''));
 }
 
-export function pickValues(object, keys = []) {
+export function pickValues(object = {}, keys = []) {
     return toArray(pick(object, keys));
 }
 
-export function toObject(entries) {
+export function toObject(entries = []) {
     return entries.reduce((object, [key, value]) => {
         object[key] = value;
 
@@ -36,6 +36,6 @@ export function toObject(entries) {
     }, {});
 }
 
-export function toArray(object) {
+export function toArray(object = {}) {
     return Object.values(object);
 }
